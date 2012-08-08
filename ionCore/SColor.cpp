1f77774258ca8801b993e29750694d9b530745a8
iondune <none@none>
2012-08-07 21:40:36 -0700

    + Added primitive types and simple typedefs, organized core
    --HG--
    branch : SFML

--- ionCore/SColor.cpp
+++
@@ -1,2 +0,0 @@
-#include "SColor.h"
-
---
+++ ionCore/ionTypes.h
@@ -0,0 +1,31 @@
+#ifndef _IONTYPES_H_INCLUDED_
+#define _IONTYPES_H_INCLUDED_
+
+#include "ionPrimitives.h"
+
+#include "SVector.h"
+#include "SVector2.h"
+#include "SVector3.h"
+#include "SColor.h"
+
+#include "SBoundingBox3.h"
+#include "SLine3.h"
+#include "SRect2.h"
+
+
+typedef SVector3f vec3;
+typedef SVector3f vec3f;
+typedef SVector3d vec3d;
+typedef SVector3i vec3i;
+
+typedef SVector2f vec2;
+typedef SVector2f vec2f;
+typedef SVector2d vec2d;
+typedef SVector2i vec2i;
+
+typedef SColorf  color;
+typedef SColori  colori;
+typedef SColorAf colora;
+typedef SColorAi colorai;
+
+#endif
