1d17b82fe87c71e3ae8c9f49671e5c95e5da1cf7
iondune <none@none>
2012-11-18 04:43:18 -0800

    + Updated camera control to work with arbitrary up vectors!

--- ionWindow/CCameraControl.cpp
+++ ionWindow/CCameraControl.cpp
@@ -1,5 +1,8 @@
 #include "CCameraControl.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
+
 CCameraControl::CCameraControl(SVector3f const position)
 	: Application(CApplication::get()), EventManager(CApplication::get().getEventManager()), Phi(0), Theta(-1.5708f), Tracking(false), MoveSpeed(20.5f)
 {
@@ -19,8 +22,8 @@ void CCameraControl::OnMouseEvent(SMouseEvent const & Event)
 	{
 		if (Tracking)
 		{
-			Theta += (Event.Movement.X)*0.005f;
-			Phi -= (Event.Movement.Y)*0.005f;
+			Theta -= (Event.Movement.X)*0.005f;
+			Phi += (Event.Movement.Y)*0.005f;
 
 			if (Phi > 3.1415f/2.02f)
 				Phi = 3.1415f/2.02f;
@@ -35,7 +38,26 @@ void CCameraControl::OnMouseEvent(SMouseEvent const & Event)
 
 void CCameraControl::update(float const TickTime)
 {
-	LookDirection = SVector3f(cos(Theta)*cos(Phi), sin(Phi), sin(Theta)*cos(Phi));
+	glm::vec4 getForward(UpVector.getGLMVector(), 1.0);
+	STransformation3 trans;
+	trans.setRotation(vec3(45));
+	glm::mat4 Rotation = trans.Rotation;
+	getForward = Rotation * getForward;
+
+	glm::vec4 Up(UpVector.getGLMVector(), 1.0);
+
+	glm::vec4 Forward(glm::normalize(glm::cross(glm::vec3(Up), glm::vec3(getForward))), 1.0);
+	Rotation = glm::rotate(glm::mat4(1.f), Theta*180.f/3.14159f, glm::vec3(Up));
+	Forward = Rotation * Forward;
+
+	glm::vec4 Side(glm::normalize(glm::cross(glm::vec3(Up), glm::vec3(Forward))), 1.0);
+	Rotation = glm::rotate(glm::mat4(1.f), Phi*180.f/3.14159f, glm::vec3(Side));
+	Forward = Rotation * Forward;
+
+	vec3 SideVector(Side.x, Side.y, Side.z);
+
+
+	LookDirection = SVector3f(Forward.x, Forward.y, Forward.z);
 
 	if (EventManager.IsKeyDown[EKey::w] || EventManager.IsKeyDown[EKey::UP])
 	{
@@ -44,14 +66,12 @@ void CCameraControl::update(float const TickTime)
 
 	if (EventManager.IsKeyDown[EKey::a] || EventManager.IsKeyDown[EKey::LEFT])
 	{
-		Translation.X += cos(Theta - 1.57079633f)*MoveSpeed*TickTime;
-		Translation.Z += sin(Theta - 1.57079633f)*MoveSpeed*TickTime;
+		Translation += SideVector*MoveSpeed*TickTime;
 	}
 
 	if (EventManager.IsKeyDown[EKey::d] || EventManager.IsKeyDown[EKey::RIGHT])
 	{
-		Translation.X += cos(Theta + 1.57079633f)*MoveSpeed*TickTime;
-		Translation.Z += sin(Theta + 1.57079633f)*MoveSpeed*TickTime;
+		Translation -= SideVector*MoveSpeed*TickTime;
 	}
 
 	if (EventManager.IsKeyDown[EKey::s] || EventManager.IsKeyDown[EKey::DOWN])
