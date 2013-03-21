4857ba67504db06fee86e21e180629ae05b270f0
Ian <none@none>
2013-03-21 16:45:39 +0100

    + Error reporting in bindBufferObject

--- ionScene/CShaderContext.cpp
+++ ionScene/CShaderContext.cpp
@@ -80,6 +80,8 @@ void CShaderContext::bindBufferObject(std::string const & label, GLuint const Bu
 	glBindBuffer(GL_ARRAY_BUFFER, BufferHandle);
 	glVertexAttribPointer(it->second.Handle, ElementSize, GL_FLOAT, GL_FALSE, 0, 0);
 
+	printOpenGLErrors("bindBufferObject(label)");
+
 	EnabledVertexAttribArrays.push_back(it->second.Handle);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -90,6 +92,8 @@ void CShaderContext::bindBufferObject(GLuint const attribHandle, GLuint const bu
 	glEnableVertexAttribArray(attribHandle); // Select Shader Attribute
 	glBindBuffer(GL_ARRAY_BUFFER, bufferHandle); // Bind Attribute Buffer
 	glVertexAttribPointer(attribHandle, elementSize, GL_FLOAT, GL_FALSE, 0, 0); // Sync Buffer Data
+
+	printOpenGLErrors("bindBufferObject(handle)");
 
 	EnabledVertexAttribArrays.push_back(attribHandle);
 
