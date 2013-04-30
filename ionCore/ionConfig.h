de1532b6ea71e8ad547044f7041f729975890b8f
Ian <none@none>
2013-04-29 19:16:57 -0700

    + Added configuration for CUDA inclusion

--- ionCore/ionConfig.h
+++ ionCore/ionConfig.h
@@ -1,7 +1,21 @@
 #ifndef _IONCORE_CONFIG_H_INCLUDED_
 #define _IONCORE_CONFIG_H_INCLUDED_
 
-#include <cuda.h>
-#include <cuda_runtime.h>
+
+//#define _ION_CONFIG_CUDA_SUPPORT_
+
+	#ifdef _ION_CONFIG_CUDA_SUPPORT_
+
+		#include <cuda.h>
+		#include <cuda_runtime.h>
+		// To Do : Add ionCUDA.h include
+		#define __universal__ __host__ __device__
+		#define ION_FUNC_DEF __universal__
+
+	#else
+
+		#define ION_FUNC_DEF
+
+	#endif
 
 #endif
