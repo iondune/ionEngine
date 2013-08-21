ffda6d9d2f3bdc493044d8ec51a161e857dad36e
Ian <none@none>
2013-08-20 20:43:17 -0700

    + Wired up window to events

--- ionWindow/CWindow.cpp
+++ ionWindow/CWindow.cpp
@@ -19,4 +19,8 @@ CWindow::CWindow(GLFWwindow * windowHandle)
 
     for (unsigned int i = 0; i < (int) SMouseEvent::EButton::Count; ++ i)
         MouseStates[i] = false;
+	
+	KeyboardEvent.AddTrigger(SEvent<SKeyboardEvent>::ITrigger::shared_from_this());
+	MouseEvent.AddTrigger(SEvent<SMouseEvent>::ITrigger::shared_from_this());
+	WindowResizedEvent.AddTrigger(SEvent<SWindowResizedEvent>::ITrigger::shared_from_this());
 }
