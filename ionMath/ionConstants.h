ac63f1cbe877e2382d156cb8878cd58d948e691d
Ian <none@none>
2013-09-02 23:45:03 -0700

    + Added e constant
    --HG--
    branch : Framework-Overhaul

--- ionMath/ionConstants.h
+++ ionMath/ionConstants.h
@@ -7,6 +7,7 @@ namespace Constants32
 {
 
 	f32 const Pi = 3.14159265359f;
+	f32 const e =  2.71828182845f;
 
 };
 
@@ -14,5 +15,6 @@ namespace Constants64
 {
 
 	f64 const Pi = 3.141592653589793238462643383279;
+	f64 const e =  2.718281828459045235360287471352;
 
 };
