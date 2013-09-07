011db0af040eae2a946c6cf40fe40af60a27f9e3
Ian <none@none>
2013-09-07 16:35:35 -0700

    + New math utility functions + Templated constants support
    --HG--
    branch : Science

---
+++ ionMath/Utilities.h
@@ -0,0 +1,70 @@
+
+#pragma once
+
+#include <ionMath.h>
+
+#include <cmath>
+
+
+template <typename T>
+T Abs(T const & value)
+{
+	return abs(value);
+}
+
+template <typename T>
+T AbsoluteValue(T const & value)
+{
+	return abs(value);
+}
+
+
+template <typename T>
+T Sin(T const & value)
+{
+	return sin(value);
+}
+
+template <typename T>
+T Cos(T const & value)
+{
+	return cos(value);
+}
+
+template <typename T>
+T ATan2(T const & value)
+{
+	return atan2(value);
+}
+
+
+template <typename T>
+T Sqrt(T const & value)
+{
+	return sqrt(value);
+}
+
+
+template <typename T>
+T DegToRad(T const & degrees)
+{
+	return degrees * Constants<T>::Pi / 180;
+}
+
+template <typename T>
+T DegreesToRadians(T const & degrees)
+{
+	return degrees * Constants<T>::Pi / 180;
+}
+
+template <typename T>
+T RadToDeg(T const & radians)
+{
+	return radians * 180 / Constants<T>::Pi;
+}
+
+template <typename T>
+T RadiansToDegrees(T const & radians)
+{
+	return radians * 180 / Constants<T>::Pi;
+}
