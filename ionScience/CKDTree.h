ca060a2e6b4017c7bbe6d35353c7067340e94a57
Ian <none@none>
2013-09-07 03:27:08 -0700

    + Updated naming of KDTree
    --HG--
    branch : Science

--- ionScience/CKDTree.h
+++ ionScience/CKDTree.h
@@ -3,52 +3,52 @@
 #include <ionMath.h>
 
 
-struct KDTreeNeighborSet
+struct CKDTreeNeighborSet
 {
 	u32 const K;
 	vec3f const Goal;
 
 	std::vector<std::pair<vec3f, f32> > Set;
 
-	KDTreeNeighborSet(u32 const k, vec3f const & goal);
+	CKDTreeNeighborSet(u32 const k, vec3f const & goal);
 
 	void ConsiderPoint(vec3f const & Point);
 	f32 GetWorstDistance() const;
 	bool Filled() const;
 };
 
-class KDTreeNode
+class CKDTreeNode
 {
 
 public:
 
-	KDTreeNode()
+	CKDTreeNode()
 		: LeftSide(), RightSide()
 	{}
 
-	KDTreeNode * LeftSide, * RightSide;
+	CKDTreeNode * LeftSide, * RightSide;
 	s32 SplitAxis;
 	vec3f Position;
 
 	void Split(vec3f * Elements, u32 const Count, s32 const Axis);
 	vec3f NearestNeighbor(vec3f const & Point) const;
-	void NearestKNeighbors(KDTreeNeighborSet & Set) const;
+	void NearestKNeighbors(CKDTreeNeighborSet & Set) const;
 
 };
 
-class KDTree
+class CKDTree
 {
 
 public:
 
-	KDTree()
+	CKDTree()
 		: Root()
 	{}
 
 	void Build(vec3f * Elements, u32 const Count);
 	vec3f NearestNeighbor(vec3f const & Point) const;
-	KDTreeNeighborSet NearestKNeighbors(vec3f const & Point, u32 const K) const;
+	CKDTreeNeighborSet NearestKNeighbors(vec3f const & Point, u32 const K) const;
 
-	KDTreeNode * Root;
+	CKDTreeNode * Root;
 
 };
