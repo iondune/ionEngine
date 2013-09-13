b50d9c9a90627d9a126dfd846677948b836276b9
Ian <none@none>
2013-09-13 03:46:54 -0700

    + Implemented vincenty ellipsoid model for long/lat conversion

--- ionScience/SLongitudeLatitude.h
+++ ionScience/SLongitudeLatitude.h
@@ -94,7 +94,7 @@ struct SLongitudeLatitude : public SVector<T, 2, SLongitudeLatitude<T> >
 
 	ION_FUNC_DEF T DistanceTo(SLongitudeLatitude const & Other) const
 	{
-		T EarthRadius = 6371;
+		T EarthRadius = (T) 6378.137;
 		T DeltaLat = DegreesToRadians(Other.Latitude - Latitude);
 		T DeltaLong = DegreesToRadians(Other.Longitude - Longitude);
 		T A = 
@@ -106,6 +106,54 @@ struct SLongitudeLatitude : public SVector<T, 2, SLongitudeLatitude<T> >
 		return Distance * 1000;
 	}
 
+	ION_FUNC_DEF T VincentyDistanceTo(SLongitudeLatitude const & Other) const
+	{
+		T const lon1 = DegreesToRadians(Longitude), lat1 = DegreesToRadians(Latitude);
+		T const lon2 = DegreesToRadians(Other.Longitude), lat2 = DegreesToRadians(Other.Latitude);
+		//T const a = (T) 6378137, b = (T) 6356752.314245,  f = (T) (1 / 298.257223563);  // WGS-84 ellipsoid params
+		T const a = (T) 6378388, b = (T) 6356911.946,  f = (T) (1 / 297.0);  // WGS-84 ellipsoid params
+		T const L = lon2 - lon1;
+		T const U1 = ArcTan((1-f) * Tan(lat1));
+		T const U2 = ArcTan((1-f) * Tan(lat2));
+		T const sinU1 = Sin(U1), cosU1 = Cos(U1);
+		T const sinU2 = Sin(U2), cosU2 = Cos(U2);
+
+		T lambda = L, lambdaP, iterLimit = 100;
+		T cosSqAlpha, sinSigma, cos2SigmaM, cosSigma, sigma;
+		do {
+			T sinLambda = Sin(lambda), cosLambda = Cos(lambda);
+			sinSigma = Sqrt((cosU2*sinLambda) * (cosU2*sinLambda) + 
+				(cosU1*sinU2-sinU1*cosU2*cosLambda) * (cosU1*sinU2-sinU1*cosU2*cosLambda));
+			if (sinSigma==0)
+				return 0; // co-incident points
+			cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda;
+			sigma = ArcTan(sinSigma, cosSigma);
+			T sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
+			cosSqAlpha = 1 - sinAlpha*sinAlpha;
+			cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha;
+			if (IsNaN(cos2SigmaM))
+				cos2SigmaM = 0;  // equatorial line: cosSqAlpha=0 (§6)
+			T C = f/16*cosSqAlpha*(4+f*(4-3*cosSqAlpha));
+			lambdaP = lambda;
+			lambda = L + (1-C) * f * sinAlpha * (sigma + C*sinSigma*(cos2SigmaM+C*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)));
+		}
+		while (Abs(lambda-lambdaP) > 1e-12 && --iterLimit>0);
+
+		if (iterLimit==0)
+		{
+			std::cerr << "Failed to converge in Long/Lat conversion." << std::endl;
+			return std::numeric_limits<T>::max();  // formula failed to converge
+		}
+
+		T uSq = cosSqAlpha * (a*a - b*b) / (b*b);
+		T A = 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)));
+		T B = uSq/1024 * (256+uSq*(-128+uSq*(74-47*uSq)));
+		T deltaSigma = B*sinSigma*(cos2SigmaM+B/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM) - B/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)));
+		T s = b*A*(sigma-deltaSigma);
+
+		return s;
+	}
+
 	enum class EOffsetMode
 	{
 		Left,
@@ -113,7 +161,7 @@ struct SLongitudeLatitude : public SVector<T, 2, SLongitudeLatitude<T> >
 		Average
 	};
 
-	ION_FUNC_DEF SVector2<T> OffsetTo(SLongitudeLatitude const & Other, EOffsetMode const Mode = EOffsetMode::Average) const
+	ION_FUNC_DEF SVector2<T> OffsetTo(SLongitudeLatitude const & Other, EOffsetMode const Mode = EOffsetMode::Average, bool const Vincenty = false) const
 	{
 		SVector2<T> Offset;
 		SLongitudeLatitude Left, Right;
@@ -133,7 +181,10 @@ struct SLongitudeLatitude : public SVector<T, 2, SLongitudeLatitude<T> >
 			Left.Latitude = Right.Latitude = Other.Latitude;
 			break;
 		}
-		Offset.X = (Left.Longitude < Right.Longitude ? 1 : -1) * Left.DistanceTo(Right);
+		if (Vincenty)
+			Offset.X = (Left.Longitude < Right.Longitude ? 1 : -1) * Left.VincentyDistanceTo(Right);
+		else
+			Offset.X = (Left.Longitude < Right.Longitude ? 1 : -1) * Left.DistanceTo(Right);
 
 		Left.Latitude = Latitude;
 		Right.Latitude = Other.Latitude;
@@ -150,7 +201,10 @@ struct SLongitudeLatitude : public SVector<T, 2, SLongitudeLatitude<T> >
 			Left.Longitude = Right.Longitude = Other.Longitude;
 			break;
 		}
-		Offset.Y = (Left.Latitude < Right.Latitude ? 1 : -1) * Left.DistanceTo(Right);
+		if (Vincenty)
+			Offset.Y = (Left.Latitude < Right.Latitude ? 1 : -1) * Left.VincentyDistanceTo(Right);
+		else
+			Offset.Y = (Left.Latitude < Right.Latitude ? 1 : -1) * Left.DistanceTo(Right);
 
 		return Offset;
 	}
