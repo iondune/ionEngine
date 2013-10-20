662211ba75d1e136e29a004bd097385a65f70dfa
Ian <none@none>
2013-10-20 03:26:14 -0700

    + Added includes for ionAsset and ionGraphics
    --HG--
    branch : SceneOverhaul

--- ionEngine.h
+++ ionEngine.h
@@ -3,9 +3,12 @@
 
 #include "ionConfig.h"
 #include "ionCore.h"
+
 #include "ionAnimation.h"
+#include "ionAsset.h"
 #include "ionFramework.h"
 #include "ionGL.h"
+#include "ionGraphics.h"
 #include "ionGUI.h"
 #include "ionMath.h"
 #include "ionScene.h"
