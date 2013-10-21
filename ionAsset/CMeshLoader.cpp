2c8f70b83485435e1f47632160bb15dd211fd510
Ian <none@none>
2013-10-21 00:46:05 -0700

    + Testing assimp
    --HG--
    branch : SceneOverhaul

--- ionAsset/CMeshLoader.cpp
+++ ionAsset/CMeshLoader.cpp
@@ -15,6 +15,34 @@
 std::map<std::string, CMesh *> CMeshLoader::LoadedMeshes;
 std::string CMeshLoader::MeshDirectory = "../Media/Models/";
 
+#include <assimp/Importer.hpp> // C++ importer interface
+#include <assimp/scene.h> // Output data structure
+#include <assimp/postprocess.h> // Post processing flags
+bool DoTheImportThing( const std::string& pFile)
+{
+	// Create an instance of the Importer class
+	Assimp::Importer importer;
+	// And have it read the given file with some example postprocessing
+	// Usually - if speed is not the most important aspect for you - you'll
+	// propably to request more postprocessing than we do in this example.
+	const aiScene* scene = importer.ReadFile( pFile,
+	aiProcess_CalcTangentSpace |
+	aiProcess_Triangulate |
+	aiProcess_JoinIdenticalVertices |
+	aiProcess_SortByPType);
+	// If the import failed, report it
+	if( !scene)
+	{
+	//DoTheErrorLogging( importer.GetErrorString());
+	return false;
+	}
+	// Now we can access the file's contents.
+	//DoTheSceneProcessing( scene);
+	// We're done. Everything will be cleaned up by the importer destructor
+	return true;
+}
+
+
 static long filelength(int f)
 {
     struct stat buf;
@@ -515,6 +543,7 @@ CMesh * const CMeshLoader::loadObjMesh(std::string const & fileName)
 
 CMesh * const CMeshLoader::createCubeMesh()
 {
+	Assimp::Importer importer;
     CMesh * MeshWrapper = new CMesh();
     CMesh::SMeshBuffer * Mesh = new CMesh::SMeshBuffer();
 
