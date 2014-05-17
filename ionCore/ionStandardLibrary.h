41110b5f7a827f293b9d7bbf1bbc2fffadfe469e
Ian Dunn <iondune@gmail.com>
2014-05-16 20:27:19 -0700

    Add map access utility
    --HG--
    branch : SceneOverhaul

--- ionCore/ionStandardLibrary.h
+++ ionCore/ionStandardLibrary.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include "ionTypes.h"
+
 #include <algorithm>
 #include <numeric>
 #include <iomanip>
@@ -9,3 +11,15 @@
 #include <sstream>
 
 using std::move;
+using std::ifstream;
+
+template <typename T, typename U>
+U * ConditionalMapAccess(map<T, U *> & Map, T const Key)
+{
+	auto Iterator = Map.find(Key);
+
+	if (Iterator != Map.end())
+		return Iterator->second;
+
+	return 0;
+}
