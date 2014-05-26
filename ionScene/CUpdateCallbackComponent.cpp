e881397b198456b0ce87c7203ac7a5662e87c922
Ian Dunn <iondune@gmail.com>
2014-05-26 05:15:22 -0700

    Add update callback component
    --HG--
    branch : SceneOverhaul

---
+++ ionScene/CUpdateCallbackComponent.cpp
@@ -0,0 +1,16 @@
+
+#include "CUpdateCallbackComponent.h"
+
+
+CUpdateCallbackComponent::CUpdateCallbackComponent(std::function<void(CSceneNode *)> const & Callback)
+{
+	this->Callback = Callback;
+}
+	
+void CUpdateCallbackComponent::Update(CSceneNode * Node)
+{
+	Callback(Node);
+}
+
+void CUpdateCallbackComponent::Draw(CSceneNode * Node, IGraphicsEngine * Engine)
+{}
