060149aab4888ad89531620f6d885683a0f8a02b
Ian Dunn <iondune@gmail.com>
2014-10-11 03:24:09 -0700

    Fix inheritance issue in ionRand and remove old func-def macros

--- ionCore/ionRand.h
+++ ionCore/ionRand.h
@@ -25,66 +25,66 @@ public:
 	virtual u32 const Generate() = 0;
 	virtual void Seed(u32 const seed) = 0;
 
-	ION_FUNC_DEF u32 const Generate(u32 const max)
+	u32 const Generate(u32 const max)
 	{
 		return Generate() % max;
 	}
 
-	ION_FUNC_DEF u32 const Generate(u32 const min, u32 const max)
+	u32 const Generate(u32 const min, u32 const max)
 	{
 		return Generate() % (max - min) + min;
 	}
 
-	ION_FUNC_DEF f32 const Generatef32(u32 const modulus = 100000)
+	f32 const Generatef32(u32 const modulus = 100000)
 	{
 		return (Generate() % modulus) / (f32) modulus;
 	}
 
-	ION_FUNC_DEF f32 const Generatef32(f32 const max, u32 const modulus = 100000)
+	f32 const Generatef32(f32 const max, u32 const modulus = 100000)
 	{
 		return Generatef32(modulus) * max;
 	}
 
-	ION_FUNC_DEF f32 const Generatef32(f32 const min, f32 const max, u32 const modulus = 100000)
+	f32 const Generatef32(f32 const min, f32 const max, u32 const modulus = 100000)
 	{
 		return Generatef32(modulus) * (max - min) + min;
 	}
 
-	ION_FUNC_DEF f64 const Generatef64(u32 const modulus = 100000)
+	f64 const Generatef64(u32 const modulus = 100000)
 	{
 		return (Generate() % modulus) / (f64) modulus;
 	}
 
-	ION_FUNC_DEF f64 const Generatef64(f64 const max, u32 const modulus = 100000)
+	f64 const Generatef64(f64 const max, u32 const modulus = 100000)
 	{
 		return Generatef64(modulus) * max;
 	}
 
-	ION_FUNC_DEF f64 const Generatef64(f64 const min, f64 const max, u32 const modulus = 100000)
+	f64 const Generatef64(f64 const min, f64 const max, u32 const modulus = 100000)
 	{
 		return Generatef64(modulus) * (max - min) + min;
 	}
 
 };
 
-class CLinearCongruentialGenerator
+class CLinearCongruentialGenerator : public IRandomGenerator
 {
 
 	u32 Value;
 
 public:
 
-	ION_FUNC_DEF CLinearCongruentialGenerator(u32 const seed = 0)
+	CLinearCongruentialGenerator(u32 const seed = 0)
 	{
 		Seed(seed);
 	}
 
-	ION_FUNC_DEF u32 const Generate()
+	u32 const Generate()
 	{
 		return (Value = (Value * 214013 + 2531011) & ((1U << 31) - 1));
 	}
 
-	ION_FUNC_DEF void Seed(u32 const seed)
+	void Seed(u32 const seed)
 	{
 		Value = seed;
 	}
