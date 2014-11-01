6eb12481619dae5148a7d4d12b2a4bc343bce349
Ian Dunn <iondune@gmail.com>
2014-11-01 04:22:30 -0700

    Start ionCore test suite

---
+++ TestCore/TestCore.cpp
@@ -0,0 +1,78 @@
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+#include <ionCore.h>
+
+
+
+class A
+{};
+
+class B : public A
+{};
+
+class C
+{
+public:
+	virtual ~C()
+	{}
+};
+
+TEST_CASE("InstanceOf", "[ionClass :: InstanceOf]")
+{
+	A a;
+	B b;
+	C c;
+	
+	REQUIRE(InstanceOf<A>(a));
+	REQUIRE(InstanceOf<A>(&a));
+	REQUIRE(InstanceOf<A>(b));
+	REQUIRE(InstanceOf<A>(&b));
+	REQUIRE(! InstanceOf<A>(c));
+	REQUIRE(! InstanceOf<A>(&c));
+	REQUIRE(NULL != As<A>(&a));
+	REQUIRE(NULL != As<A>(&b));
+	REQUIRE(NULL == As<A>(&c));
+}
+
+TEST_CASE("Type", "[ionClass :: Type]")
+{
+	Type TypeA(typeid(A));
+	Type TypeB(typeid(B));
+	Type TypeC(typeid(C));
+	
+	REQUIRE((TypeA < TypeB) != (TypeB < TypeA));
+	REQUIRE((TypeA < TypeC) != (TypeC < TypeA));
+	REQUIRE((TypeB < TypeC) != (TypeC < TypeB));
+}
+
+class F;
+
+class E : public Singleton<E>
+{
+public:
+	SingletonPointer<F> FPtr;
+};
+
+class F : public Singleton<F>
+{
+public:
+	SingletonPointer<E> EPtr;
+};
+
+TEST_CASE("Singleton lazy initialization", "[ionClass :: Singleton]")
+{
+	SingletonPointer<E> e;
+	SingletonPointer<F> f;
+	
+	REQUIRE(e->FPtr.Get() == f.Get());
+	REQUIRE(f->EPtr.Get() == e.Get());
+}
+
+int main(int argc, char * const argv[])
+{
+	int result = Catch::Session().run(argc, argv);
+	system("PAUSE");
+	return result;
+}
