059ce96aab77b591688090968aa9f42191afacf6
Ian Dunn <iondune@gmail.com>
2014-11-06 01:38:02 -0500

    Clean output from Analytics/Generate script

--- Analytics/Generate.sh
+++ Analytics/Generate.sh
@@ -6,6 +6,7 @@ cd ..
 RootDirectory=$(pwd)
 
 # Build
+echo "Building..."
 cd "$RootDirectory"
 ./SetConfiguration.sh -c
 make --quiet
@@ -15,22 +16,27 @@ if [ $? -ne 0 ] ; then
 fi
 
 # Initial
+echo "Generating basline zero coverage..."
 cd "$ScriptDirectory"
 rm -f *.info
-lcov --capture --no-external --directory /home/ian/Projects/ionEngine/ --initial --output-file baseline.info
+lcov --capture --no-external --directory /home/ian/Projects/ionEngine/ --initial --output-file baseline.info --quiet
 
 # Run
+echo "Running tests..."
 "${RootDirectory}/RunTests.sh"
 
 # Coverage
-lcov --capture --no-external --directory /home/ian/Projects/ionEngine/ --output-file coverage.info
+echo "Running coverage..."
+lcov --capture --no-external --directory /home/ian/Projects/ionEngine/ --output-file coverage.info --quiet
 
 # Combine
-lcov --add-tracefile coverage.info --add-tracefile baseline.info --output-file combined.info
-lcov --remove combined.info 'Instantiate.h' --output-file final.info
+echo "Finalizing coverage data..."
+lcov --add-tracefile coverage.info --add-tracefile baseline.info --output-file combined.info --quiet
+lcov --remove combined.info 'Instantiate.h' --output-file final.info --quiet
 
 # Report
+echo "Generating report..."
 rm -r html
-genhtml final.info --output-directory html
+genhtml final.info --output-directory html --quiet
 rm -r /var/www/amethyst.iondune.net/public_html/cov
 cp -r html /var/www/amethyst.iondune.net/public_html/cov
