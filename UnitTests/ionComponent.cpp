4ddb397eabb5ea99415ad0318c79dc8f4cdc843d
Ian Dunn <iondune@gmail.com>
2014-11-14 12:38:39 -0500

    Add IEntity::GetComponentCount test

--- UnitTests/ionComponent.cpp
+++ UnitTests/ionComponent.cpp
@@ -24,7 +24,41 @@ TEST_CASE("IEntity::AddComponent")
 	AComponent * A2 = new CComponent();
 
 	REQUIRE(A == Entity->AddComponent(A));
+	REQUIRE(A == Entity->AddComponent(A));
 	REQUIRE(B == Entity->AddComponent(B));
 	REQUIRE(C == Entity->AddComponent(C));
 	REQUIRE(A2 == Entity->AddComponent(A2));
+}
+
+TEST_CASE("IEntity::GetComponentCount")
+{
+	IEntity<Component> * Entity = new IEntity<Component>();
+	AComponent * A = new AComponent();
+	BComponent * B = new BComponent();
+	CComponent * C = new CComponent();
+	AComponent * A2 = new CComponent();
+
+	REQUIRE(Entity->GetComponentCount<AComponent>() == 0);
+	REQUIRE(Entity->GetComponentCount<BComponent>() == 0);
+	REQUIRE(Entity->GetComponentCount<CComponent>() == 0);
+	Entity->AddComponent(A);
+	REQUIRE(Entity->GetComponentCount<AComponent>() == 1);
+	REQUIRE(Entity->GetComponentCount<BComponent>() == 0);
+	REQUIRE(Entity->GetComponentCount<CComponent>() == 0);
+	Entity->AddComponent(A);
+	REQUIRE(Entity->GetComponentCount<AComponent>() == 2);
+	REQUIRE(Entity->GetComponentCount<BComponent>() == 0);
+	REQUIRE(Entity->GetComponentCount<CComponent>() == 0);
+	Entity->AddComponent(B);
+	REQUIRE(Entity->GetComponentCount<AComponent>() == 2);
+	REQUIRE(Entity->GetComponentCount<BComponent>() == 1);
+	REQUIRE(Entity->GetComponentCount<CComponent>() == 0);
+	Entity->AddComponent(C);
+	REQUIRE(Entity->GetComponentCount<AComponent>() == 2);
+	REQUIRE(Entity->GetComponentCount<BComponent>() == 1);
+	REQUIRE(Entity->GetComponentCount<CComponent>() == 1);
+	Entity->AddComponent(A2);
+	REQUIRE(Entity->GetComponentCount<AComponent>() == 3);
+	REQUIRE(Entity->GetComponentCount<BComponent>() == 1);
+	REQUIRE(Entity->GetComponentCount<CComponent>() == 1);
 }
