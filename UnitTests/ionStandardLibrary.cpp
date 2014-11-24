f6a12ae9562fbba54ddc4d6ca6f0f997e33ae68b
Ian Dunn <iondune@gmail.com>
2014-11-24 12:36:30 -0500

    Add Conditional Map Access test

--- UnitTests/ionStandardLibrary.cpp
+++ UnitTests/ionStandardLibrary.cpp
@@ -3,6 +3,26 @@
 #include <catch.hpp>
 
 
+TEST_CASE("ionStandardLibrary::ConditionalMapAccess")
+{
+	map<int, int *> Test;
+	REQUIRE(! ConditionalMapAccess(Test, 0));
+	REQUIRE(! ConditionalMapAccess(Test, 1));
+	REQUIRE(! ConditionalMapAccess(Test, 2));
+
+	int X, Y;
+
+	Test[0] = & X;
+	Test[1] = & Y;
+	Test[2] = & Y;
+	REQUIRE(! ConditionalMapAccess(Test, -1));
+	REQUIRE(& X == ConditionalMapAccess(Test, 0));
+	REQUIRE(& Y == ConditionalMapAccess(Test, 1));
+	REQUIRE(& Y == ConditionalMapAccess(Test, 2));
+	REQUIRE(! ConditionalMapAccess(Test, 3));
+	REQUIRE(! ConditionalMapAccess(Test, 4));
+}
+
 TEST_CASE("ionStandardLibrary::String::Build")
 {
 	REQUIRE(String::Build("") == string());
