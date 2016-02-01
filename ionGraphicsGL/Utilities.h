82b04a0f3c930dc1805fb8fa3001958683964a6a
Ian Dunn <iondune@gmail.com>
2016-02-01 03:55:57 -0800

    Cleanup in ionGraphicsGL

--- ionGraphicsGL/Utilities.h
+++ ionGraphicsGL/Utilities.h
@@ -15,51 +15,51 @@ namespace ion
 
 			int GetValueTypeOpenGLEnum(EValueType const ValueType);
 
-		bool OpenGLError();
-		string GetOpenGLError();
-		void PrintOpenGLErrors();
-		void PrintOpenGLErrors(c8 const * const Function, c8 const * const File, s32 const Line);
+			bool OpenGLError();
+			string GetOpenGLError();
+			void PrintOpenGLErrors();
+			void PrintOpenGLErrors(c8 const * const Function, c8 const * const File, s32 const Line);
 
 #ifdef _DEBUG
-		#define CheckedGLCall(x) do { ion::Graphics::GL::PrintOpenGLErrors(">>BEFORE<< "#x, __FILE__, __LINE__); (x); ion::Graphics::GL::PrintOpenGLErrors(#x, __FILE__, __LINE__); } while (0)
-		#define CheckExistingErrors(x) ion::Graphics::GL::PrintOpenGLErrors(">>BEFORE<< "#x, __FILE__, __LINE__);
+			#define CheckedGLCall(x) do { ion::Graphics::GL::PrintOpenGLErrors(">>BEFORE<< "#x, __FILE__, __LINE__); (x); ion::Graphics::GL::PrintOpenGLErrors(#x, __FILE__, __LINE__); } while (0)
+			#define CheckExistingErrors(x) ion::Graphics::GL::PrintOpenGLErrors(">>BEFORE<< "#x, __FILE__, __LINE__);
 #else
-		#define CheckedGLCall(x) (x)
-		#define CheckExistingErrors(x)
+			#define CheckedGLCall(x) (x)
+			#define CheckExistingErrors(x)
 #endif
 
-		enum class EFormatType
-		{
-			U8 = 0,
-			U16 = 1,
-			U32 = 2,
-			S8 = 3,
-			S16 = 4,
-			S32 = 5,
-			F32 = 6
-		};
+			enum class EFormatType
+			{
+				U8 = 0,
+				U16 = 1,
+				U32 = 2,
+				S8 = 3,
+				S16 = 4,
+				S32 = 5,
+				F32 = 6
+			};
 
-		enum class EPrimitiveType
-		{
-			Points = 0,
-			Lines = 1,
-			Triangles = 2,
-			Quads = 3
-		};
+			enum class EPrimitiveType
+			{
+				Points = 0,
+				Lines = 1,
+				Triangles = 2,
+				Quads = 3
+			};
 
-		class Util
-		{
-		public:
+			class Util
+			{
+			public:
 
-			static u32 const TypeMatrix[7];
-			static u64 const SizeMatrix[7];
-			static u32 const PrimitiveMatrix[4];
-			static uint const PrimitiveVertices[4];
+				static u32 const TypeMatrix[7];
+				static u64 const SizeMatrix[7];
+				static u32 const PrimitiveMatrix[4];
+				static uint const PrimitiveVertices[4];
 
-			static string const TypeStringMatrix[7];
-			static string const SizeStringMatrix[7];
-			static string const PrimitiveStringMatrix[4];
-		};
-	}
+				static string const TypeStringMatrix[7];
+				static string const SizeStringMatrix[7];
+				static string const PrimitiveStringMatrix[4];
+			};
+		}
 	}
 }
