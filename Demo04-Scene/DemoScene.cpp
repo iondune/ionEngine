a5a11eed60f68506f7c665f168f5067c84ba4504
Ian Dunn <iondune@gmail.com>
2016-02-06 17:51:30 -0800

    Add ionScene to linux build, add Demo04-Scene [non-compiling]

---
+++ Demo04-Scene/DemoScene.cpp
@@ -0,0 +1,44 @@
+
+#include <ionWindow.h>
+#include <ionGraphics.h>
+#include <ionScene.h>
+
+using namespace ion;
+using namespace Scene;
+using namespace Graphics;
+
+
+int main()
+{
+	////////////////////
+	// ionEngine Init //
+	////////////////////
+
+	Log::AddDefaultOutputs();
+
+	SingletonPointer<CWindowManager> WindowManager;
+	SingletonPointer<CSceneManager> SceneManager;
+
+	WindowManager->Init();
+	CWindow * Window = WindowManager->CreateWindow(vec2i(640, 480), "DemoScene", EWindowType::Windowed);
+
+
+	IGraphicsAPI * GraphicsAPI = new COpenGLAPI();
+
+	CSimpleMesh * Mesh = CGeometryCreator::CreateSphere();
+
+
+	ICamera * Camera = nullptr;
+	SceneManager->GetScene()->SetActiveCamera(Camera = SceneManager->GetFactory()->AddPerspectiveCamera((Window->GetAspectRatio())));
+	Camera->SetPosition(vec3f(0, 0, -3));
+
+	ion::GL::Context::Init();
+	while (! WindowManager->ShouldClose())
+	{
+		WindowManager->PollEvents();
+		SceneManager->DrawAll();
+		Window->SwapBuffers();
+	}
+
+	return 0;
+}
