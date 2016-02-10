65022ebcb23ff3e0d9dcaaaf40af7b9affe1120d
Ian Dunn <iondune@gmail.com>
2016-02-09 18:31:49 -0800

    Clean up scene demo, add multiple objects

--- Demo04-Scene/DemoScene.cpp
+++ Demo04-Scene/DemoScene.cpp
@@ -23,10 +23,13 @@ int main()
 	WindowManager->Init();
 	CWindow * Window = WindowManager->CreateWindow(vec2i(1600, 900), "DemoScene", EWindowType::Windowed);
 
-
 	IGraphicsAPI * GraphicsAPI = new COpenGLAPI();
 
-	CSimpleMesh * Mesh = CGeometryCreator::CreateSphere();
+
+	///////////////////
+	// Create Shader //
+	///////////////////
+
 	IVertexShader * VertexShader = GraphicsAPI->CreateVertexShaderFromFile("Diffuse.vert");
 	IPixelShader * PixelShader = GraphicsAPI->CreatePixelShaderFromFile("Diffuse.frag");
 
@@ -40,20 +43,54 @@ int main()
 	ShaderProgram->SetVertexStage(VertexShader);
 	ShaderProgram->SetPixelStage(PixelShader);
 
+	
+	////////////////////
+	// ionScene Setup //
+	////////////////////
+
 	CRenderPass * RenderPass = new CRenderPass(GraphicsAPI);
 	SceneManager->AddRenderPass(RenderPass);
 
-	CSimpleMeshSceneObject * SceneObject = new CSimpleMeshSceneObject();
-	SceneObject->SetMesh(Mesh);
-	SceneObject->SetShader(ShaderProgram);
-	RenderPass->AddSceneObject(SceneObject);
-
 	CPerspectiveCamera * Camera = new CPerspectiveCamera(Window->GetAspectRatio());
-	Camera->SetPosition(vec3f(0, 0, -3));
+	Camera->SetPosition(vec3f(0, 2, -4));
+	Camera->SetLookAtTarget(vec3f(0, 0, 0));
+	Camera->SetFocalLength(0.4f);
 	RenderPass->SetActiveCamera(Camera);
 
 	IRenderTarget * RenderTarget = GraphicsAPI->GetWindowBackBuffer(Window);
 	RenderTarget->SetClearColor(color3f(0.3f));
+
+
+	///////////////////
+	// Scene Objects //
+	///////////////////
+
+	CSimpleMesh * Mesh1 = CGeometryCreator::CreateSphere();
+	CSimpleMeshSceneObject * SceneObject1 = new CSimpleMeshSceneObject();
+	SceneObject1->SetMesh(Mesh1);
+	SceneObject1->SetShader(ShaderProgram);
+	SceneObject1->SetPosition(vec3f(2, 0, 0));
+	RenderPass->AddSceneObject(SceneObject1);
+
+	CSimpleMesh * Mesh2 = CGeometryCreator::CreateCylinder(0.6f, 0.3f);
+	CSimpleMeshSceneObject * SceneObject2 = new CSimpleMeshSceneObject();
+	SceneObject2->SetMesh(Mesh2);
+	SceneObject2->SetShader(ShaderProgram);
+	SceneObject2->SetPosition(vec3f(0, -0.5f, 0));
+	SceneObject2->SetRotation(vec3f(-Constants32::Pi / 2, 0, 0));
+	RenderPass->AddSceneObject(SceneObject2);
+
+	CSimpleMesh * Mesh3 = CGeometryCreator::CreateCube();
+	CSimpleMeshSceneObject * SceneObject3 = new CSimpleMeshSceneObject();
+	SceneObject3->SetMesh(Mesh3);
+	SceneObject3->SetShader(ShaderProgram);
+	SceneObject3->SetPosition(vec3f(-2, 0, 0));
+	RenderPass->AddSceneObject(SceneObject3);
+
+
+	///////////////
+	// Main Loop //
+	///////////////
 
 	while (WindowManager->Run())
 	{
