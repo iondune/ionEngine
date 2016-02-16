1337f46ce78d014f358a479f2836b59a2d22c2a0
Ian Dunn <iondune@gmail.com>
2016-02-16 00:30:25 -0500

    Update dependencies and build for unix to include glad

--- UpdateDependencies.bash
+++ UpdateDependencies.bash
@@ -8,7 +8,6 @@ function CloneGithubDependency {
 	repoName="$1"
 	repoURL="$2"
 	revision="$3"
-	buildStep="$4"
 
 	echo "Cloning Dependency '$repoName'"
 	echo "================================================================================"
@@ -17,10 +16,6 @@ function CloneGithubDependency {
 	git clone "$repoURL" "$repoName"
 	cd "./$repoName"
 	git reset --hard "$revision"
-
-	if [ -z "$4" ] ; then
-		echo "Peforming build step ''"
-	fi
 	cd ..
 
 	echo
@@ -31,3 +26,4 @@ CloneGithubDependency 'tinyformat' 'https://github.com/c42f/tinyformat.git'   'f
 CloneGithubDependency 'glm'        'https://github.com/g-truc/glm.git'        '8f0d85460557dc5fbf4f44dc23a7708d065d7683'
 CloneGithubDependency 'imgui'      'https://github.com/ocornut/imgui.git'     'b329a36d2707a94806f85e6d3af3b10c242f6440'
 CloneGithubDependency 'Catch'      'https://github.com/philsquared/Catch.git' '3bd20bf2cd05cd88e767ec1aeb856bf8032a04c7'
+CloneGithubDependency 'glad'       'https://github.com/iondune/glad.git'      'f3b5b3b5e61c66904943217bef4b1910e11bb15b'
