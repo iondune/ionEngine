31d368d62527073ac17a1096370820a3c0084f7a
Ian Dunn <iondune@gmail.com>
2016-02-21 23:20:37 -0800

    [ionAnimation] Add 'move' methods for vec3f

--- ionAnimation/Move.cpp
+++ ionAnimation/Move.cpp
@@ -30,5 +30,57 @@ namespace ion
 			return From;
 		}
 
+		vec3f Move::Linear(vec3f From, vec3f const & To, float const Elapsed, float const Speed, float const Clamp)
+		{
+			vec3f const DirectionVector = To - From;
+			float const DistanceToGo = DirectionVector.Length();
+
+			if (DistanceToGo < Clamp)
+			{
+				From = To;
+			}
+			else
+			{
+				float const DistanceCanGo = Speed * Elapsed;
+
+				if (DistanceCanGo > DistanceToGo)
+				{
+					From = To;
+				}
+				else
+				{
+					From += DirectionVector.GetNormalized() * DistanceCanGo;
+				}
+			}
+
+			return From;
+		}
+
+		vec3f Move::Quadratic(vec3f From, vec3f const & To, float const Elapsed, float const Speed, float const Clamp)
+		{
+			vec3f const DirectionVector = To - From;
+			float const DistanceToGo = DirectionVector.Length();
+
+			if (DistanceToGo < Clamp)
+			{
+				From = To;
+			}
+			else
+			{
+				float const DistanceCanGo = Speed * Elapsed * DistanceToGo;
+
+				if (DistanceCanGo > DistanceToGo)
+				{
+					From = To;
+				}
+				else
+				{
+					From += DirectionVector.GetNormalized() * DistanceCanGo;
+				}
+			}
+
+			return From;
+		}
+
 	}
 }
--- ionAnimation/Move.h
+++ ionAnimation/Move.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <ionCore.h>
+#include <ionMath.h>
 
 
 namespace ion
@@ -15,6 +16,8 @@ namespace ion
 		public:
 
 			static float Linear(float From, float const To, float const Elapsed, float const Speed, float const Clamp = 0.05f);
+			static vec3f Linear(vec3f From, vec3f const & To, float const Elapsed, float const Speed, float const Clamp = 0.05f);
+			static vec3f Quadratic(vec3f From, vec3f const & To, float const Elapsed, float const Speed, float const Clamp = 0.05f);
 
 		};
 
