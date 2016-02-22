8cac90d5841f913f574a5d14018df35b39250641
Ian Dunn <iondune@gmail.com>
2016-02-21 23:06:51 -0800

    [ionFramework] Add method to find all files with certain extension in a directory

--- ionFramework/ionDirectory.cpp
+++ ionFramework/ionDirectory.cpp
@@ -24,3 +24,23 @@ vector<string> Directory::Read(string const & Path)
 
 	return Files;
 }
+
+vector<string> Directory::FindByExtension(string const & Path, string const & Extension)
+{
+	vector<string> FoundFiles;
+	vector<string> const Files = Directory::Read(Path);
+
+	for (auto const & FileName : Files)
+	{
+		if (FileName == "." || FileName == ".." || FileName.find('.') == std::string::npos)
+			continue;
+
+		std::string const ThisFileExtension = FileName.substr(FileName.find_last_of('.'));
+		if (ThisFileExtension == ("." + Extension))
+		{
+			FoundFiles.push_back(FileName);
+		}
+	}
+
+	return FoundFiles;
+}
--- ionFramework/ionDirectory.h
+++ ionFramework/ionDirectory.h
@@ -10,5 +10,6 @@ class Directory
 public:
 
 	static vector<string> Read(string const & Path);
+	static vector<string> FindByExtension(string const & Path, string const & Extension);
 
 };
