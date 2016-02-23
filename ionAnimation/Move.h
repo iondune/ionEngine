ed03ebf674bedf106417d9c3f2445224a6e13db2
Ian Dunn <iondune@gmail.com>
2016-02-23 01:46:11 -0800

    [ionAnimation] Add cubic vec3f mover

--- ionAnimation/Move.h
+++ ionAnimation/Move.h
@@ -18,6 +18,7 @@ namespace ion
 			static float Linear(float From, float const To, float const Elapsed, float const Speed, float const Clamp = 0.05f);
 			static vec3f Linear(vec3f From, vec3f const & To, float const Elapsed, float const Speed, float const Clamp = 0.05f);
 			static vec3f Quadratic(vec3f From, vec3f const & To, float const Elapsed, float const Speed, float const Clamp = 0.05f);
+			static vec3f Cubic(vec3f From, vec3f const & To, float const Elapsed, float const Speed, float const Clamp = 0.05f);
 
 		};
 
