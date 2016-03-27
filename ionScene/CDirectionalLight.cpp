275e2d4b3054a45b5e293e8172e1ffa2eef87b26
Ian Dunn <iondune@gmail.com>
2016-03-26 19:21:40 -0700

    [ionScene] Fix directional light defaults

--- ionScene/CDirectionalLight.cpp
+++ ionScene/CDirectionalLight.cpp
@@ -15,22 +15,22 @@ namespace ion
 
 		color3f CDirectionalLight::GetColor() const
 		{
-			return *Color;
+			return Color;
 		}
 
 		vec3f CDirectionalLight::GetDirection() const
 		{
-			return *Direction;
+			return Direction;
 		}
 
 		void CDirectionalLight::SetColor(color3f const & Color)
 		{
-			*(this->Color) = Color;
+			this->Color = Color;
 		}
 
 		void CDirectionalLight::SetDirection(vec3f const & Direction)
 		{
-			*(this->Direction) = Direction;
+			this->Direction = Direction;
 		}
 
 		string CDirectionalLight::GetLightType() const
