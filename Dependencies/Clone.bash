7e0232154909edb48ff8d50f8ab51da6d72c7574
Ian Dunn <iondune@gmail.com>
2016-03-31 21:46:30 -0700

    Update tinyformat to fix clang compiler warning
    --HG--
    branch : stable

--- Dependencies/Clone.bash
+++ Dependencies/Clone.bash
@@ -20,7 +20,7 @@ function CloneGithubDependency {
 }
 
 CloneGithubDependency 'stb'            'https://github.com/nothings/stb.git'         '955dfe991b26f6fb1287ec0093f606843487b099'
-CloneGithubDependency 'tinyformat'     'https://github.com/c42f/tinyformat.git'      'fd72f9cf17e4b12cbca932049a17badc7af6718b'
+CloneGithubDependency 'tinyformat'     'https://github.com/c42f/tinyformat.git'      '96de1a131f54c132a8d3cfaff899d939e828c389'
 CloneGithubDependency 'glm'            'https://github.com/g-truc/glm.git'           '8f0d85460557dc5fbf4f44dc23a7708d065d7683'
 CloneGithubDependency 'imgui'          'https://github.com/ocornut/imgui.git'        'b329a36d2707a94806f85e6d3af3b10c242f6440'
 CloneGithubDependency 'Catch'          'https://github.com/philsquared/Catch.git'    '3bd20bf2cd05cd88e767ec1aeb856bf8032a04c7'
