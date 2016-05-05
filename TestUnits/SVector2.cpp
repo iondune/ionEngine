ca1b52d7a5e573859b5a8e67afb469a728c6845e
Ian Dunn <iondune@gmail.com>
2016-05-04 18:26:02 -0700

    [TestUnits] Additional tests for vec2i usage in a map

--- TestUnits/SVector2.cpp
+++ TestUnits/SVector2.cpp
@@ -32,4 +32,28 @@ TEST_CASE("SVector2 used as a map index (uses operator <)")
 	CHECK(it->first.X == 0);
 	CHECK(it->first.Y == -1);
 	CHECK(it->second == 4);
+
+	Map[vec2i(-1, -1)] = 5;
+	CHECK(Map[vec2i(-1, -1)] == 5);
+	Map[vec2i(1, 1)] = 7;
+	CHECK(Map[vec2i(1, 1)] == 7);
+	Map[vec2i(2, 1)] = 8;
+	CHECK(Map[vec2i(2, 1)] == 8);
+	Map[vec2i(1, 2)] = 9;
+	CHECK(Map[vec2i(1, 2)] == 9);
+
+	for (int t = -20; t <= 20; ++ t)
+	{
+		for (int q = -20; q <= 20; ++ q)
+		{
+			Map[vec2i(t, q)] = 13 + 50 * (t + 21) + (q + 21);
+		}
+	}
+	for (int t = -20; t <= 20; ++ t)
+	{
+		for (int q = -20; q <= 20; ++ q)
+		{
+			CHECK(Map[vec2i(t, q)] == 13 + 50 * (t + 21) + (q + 21));
+		}
+	}
 }
