b22601ad28e6d3fd6270b982cbbf6a737b097246
Ian Dunn <iondune@gmail.com>
2017-04-25 00:07:00 -0700

    [ionGUI] Add RAII helper for imgui ID namespaces

--- ionGUI/ionGUI.cpp
+++ ionGUI/ionGUI.cpp
@@ -2,57 +2,71 @@
 #include "ionGUI.h"
 
 
-bool ImGui::SliderDouble(const char * label, double * v, double v_min, double v_max, const char * display_format, double power)
+namespace ImGui
 {
-	float temp = (float) *v;
-	if (SliderFloat(label, &temp, (float) v_min, (float) v_max, display_format, (float) power))
+
+	bool SliderDouble(const char * label, double * v, double v_min, double v_max, const char * display_format, double power)
 	{
-		*v = (double) temp;
-		return true;
+		float temp = (float) *v;
+		if (SliderFloat(label, &temp, (float) v_min, (float) v_max, display_format, (float) power))
+		{
+			*v = (double) temp;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
-	else
+
+	bool ColorEdit3(const char * label, color3f & Color)
 	{
+		float c[3] = { Color.Red, Color.Green, Color.Blue };
+		if (ImGui::ColorEdit3(label, c))
+		{
+			Color = color3f(c[0], c[1], c[2]);
+			return true;
+		}
 		return false;
 	}
-}
 
-bool ImGui::ColorEdit3(const char * label, color3f & Color)
-{
-	float c[3] = { Color.Red, Color.Green, Color.Blue };
-	if (ImGui::ColorEdit3(label, c))
+	bool DragVec2(const char * label, vec2f & v, float v_speed, float v_min, float v_max, const char * display_format, float power)
 	{
-		Color = color3f(c[0], c[1], c[2]);
-		return true;
+		float vals[2] = { v.X, v.Y };
+		if (ImGui::DragFloat2(label, vals, v_speed, v_min, v_max, display_format, power))
+		{
+			v = vec2f(vals[0], vals[1]);
+			return true;
+		}
+		return false;
 	}
-	return false;
-}
 
-bool ImGui::DragVec2(const char * label, vec2f & v, float v_speed, float v_min, float v_max, const char * display_format, float power)
-{
-	float vals[2] = { v.X, v.Y };
-	if (ImGui::DragFloat2(label, vals, v_speed, v_min, v_max, display_format, power))
+	bool DragVec3(const char * label, vec3f & v, float v_speed, float v_min, float v_max, const char * display_format, float power)
 	{
-		v = vec2f(vals[0], vals[1]);
-		return true;
+		float vals[3] = { v.X, v.Y, v.Z };
+		if (ImGui::DragFloat3(label, vals, v_speed, v_min, v_max, display_format, power))
+		{
+			v = vec3f(vals[0], vals[1], vals[2]);
+			return true;
+		}
+		return false;
 	}
-	return false;
-}
 
-bool ImGui::DragVec3(const char * label, vec3f & v, float v_speed, float v_min, float v_max, const char * display_format, float power)
-{
-	float vals[3] = { v.X, v.Y, v.Z };
-	if (ImGui::DragFloat3(label, vals, v_speed, v_min, v_max, display_format, power))
+	bool Combo(const char * label, int * current_item, std::initializer_list<char const *> const & items, int height_in_items)
 	{
-		v = vec3f(vals[0], vals[1], vals[2]);
-		return true;
+		vector<char const *> items_vec = items;
+		return ImGui::Combo(label, current_item, items_vec.data(), (int) items.size(), height_in_items);
 	}
-	return false;
-}
 
-bool ImGui::Combo(const char * label, int * current_item, std::initializer_list<char const *> const & items, int height_in_items)
-{
-	vector<char const *> items_vec = items;
-	return ImGui::Combo(label, current_item, items_vec.data(), (int) items.size(), height_in_items);
+	scoped_id::scoped_id(char const * const str_id)
+	{
+		ImGui::PushID(str_id);
+	}
+
+	scoped_id::~scoped_id()
+	{
+		ImGui::PopID();
+	}
 }
 
 char const * ion::BoolToString(bool const B)
