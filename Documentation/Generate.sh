ce068197af316b8cc2c2461563fe46cd58d4c112
Ian Dunn <iondune@gmail.com>
2017-05-10 01:25:53 +0000

    Clean up doxyfile and update docs generation script

--- Documentation/Generate.sh
+++ Documentation/Generate.sh
@@ -3,4 +3,12 @@
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$ScriptDirectory"
 
+rm -rf xml doxy
+
+make clean
+
 doxygen > /dev/null
+
+~/ext/doxyrest_b/build/doxyrest/bin/Release/doxyrest  xml/index.xml -o doxy/index.rst -F ~/ext/doxyrest_b/doxyrest/frame/ -f c_index.rst.in
+
+make html
