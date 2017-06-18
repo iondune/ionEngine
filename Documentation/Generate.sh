b88e42286c6bef93ef110ff34aec50b7d1007581
Ian Dunn <iondune@gmail.com>
2017-06-18 16:37:00 -0700

    Scrap doxyrest/sphinx documentation, import GLFW style rules for plain doxygen output

--- Documentation/Generate.sh
+++ Documentation/Generate.sh
@@ -3,12 +3,6 @@
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$ScriptDirectory"
 
-rm -rf xml doxy
-
-make clean
-
+rm -rf html
+lessc extra.less extra.css
 doxygen > /dev/null
-
-~/ext/doxyrest_b/build/doxyrest/bin/Release/doxyrest  xml/index.xml -o doxy/index.rst -F ~/ext/doxyrest_b/doxyrest/frame/ -f c_index.rst.in
-
-make html
