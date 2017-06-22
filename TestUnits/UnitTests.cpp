b737062129bd8ad0b68c63ff066ccc75018a1ca8
Ian Dunn <iondune@gmail.com>
2017-06-22 03:06:27 -0700

    [TestUnits] Use ionCore alternative to system("PAUSE")

--- TestUnits/UnitTests.cpp
+++ TestUnits/UnitTests.cpp
@@ -2,13 +2,15 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+#include <ionCore.h>
+
 
 int main(int argc, char * const argv[])
 {
 	int result = Catch::Session().run(argc, argv);
 
 #ifdef WIN32
-	system("PAUSE");
+	ion::WaitForUser();
 #endif
 
 	return result;
