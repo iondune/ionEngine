8530a62956bd44741c67c55221be5903741d46a7
Ian Dunn <iondune@gmail.com>
2017-07-02 05:26:55 -0700

    [TestUnits] Write output to VS debug log

--- TestUnits/UnitTests.cpp
+++ TestUnits/UnitTests.cpp
@@ -2,3 +2,51 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
+#include <ionConfig.h>
+
+
+#ifdef ION_CONFIG_WINDOWS
+class DebugOutBuffer : public std::stringbuf
+{
+
+public:
+
+	virtual int sync()
+	{
+		OutputDebugString(str().c_str());
+		str("");
+		return 0;
+	}
+
+};
+
+namespace Catch
+{
+	std::ostream buffer(new DebugOutBuffer());
+
+	std::ostream & cout()
+	{
+		return buffer;
+	}
+
+	std::ostream & cerr()
+	{
+		return buffer;
+	}
+
+}
+#else
+namespace Catch
+{
+	std::ostream & cout()
+	{
+		return std::cout;
+	}
+
+	std::ostream & cerr()
+	{
+		return std::cerr;
+	}
+
+}
+#endif
