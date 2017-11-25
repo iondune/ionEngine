4919acbd98727a2f78e41fca1fedb766d1971e27
Ian Dunn <iondune@gmail.com>
2017-11-25 04:50:45 -0800

    Attempt to fix travis build by using shared libs on OS X, static on Linux

--- ContinuousIntegration/InstallGLFW.bash
+++ ContinuousIntegration/InstallGLFW.bash
@@ -4,6 +4,8 @@ git clone https://github.com/glfw/glfw
 cd glfw/
 mkdir build
 cd build/
-cmake -DBUILD_SHARED_LIBS=ON ..
+if [ "$1" = "--shared" ]; then
+	cmake -DBUILD_SHARED_LIBS=ON ..
+fi
 make -j4
 sudo make install
