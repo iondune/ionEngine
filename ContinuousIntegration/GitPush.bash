c6c95d7f0c3880b80bbd4adf39e9c8332f525f22
Ian Dunn <iondune@gmail.com>
2018-12-28 05:54:35 -0800

    Remove some miscellaneous and linux-oriented files

--- ContinuousIntegration/GitPush.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd "${0%/*}"
-cd ..
-
-hg bookmark --rev stable release
-hg bookmark --rev default master
-hg push git+ssh://git@github.com:iondune/ionEngine.git
--- ContinuousIntegration/Sync.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$ScriptDirectory"
-cp hgrc ../.hg/hgrc
-
-cd ..
-
-/home/ian/.dotfiles/bin/SyncAndGitPushHgChanges.sh .
