01d74b669af6d736c0a995dea973a002884916b7
Ian Dunn <iondune@gmail.com>
2021-02-28 11:35:31 -0800

    [Dependencies] Update onqtam/doctest to 2.4.5

--- Dependencies/Clone.bat
+++ Dependencies/Clone.bat
@@ -5,7 +5,7 @@ CALL :Clone stb             https://github.com/nothings/stb.git          b42009b
 CALL :Clone tinyformat      https://github.com/c42f/tinyformat.git       705e3f4e1de922069bf715746d35bd2364b1f98f
 CALL :Clone glm             https://github.com/g-truc/glm.git            bf71a834948186f4097caa076cd2663c69a10e1e
 CALL :Clone imgui           https://github.com/ocornut/imgui.git         4b4be11fd2dabc7a72d749c22a5eeeb3647078bc
-CALL :Clone doctest         https://github.com/onqtam/doctest.git        d5aa2bfb8f00b6260296a754af3a3a98d93f7b67
+CALL :Clone doctest         https://github.com/onqtam/doctest.git        f0ab4e793fa863d4a5929ff263ddd3bebec8767e
 CALL :Clone glad            https://github.com/iondune/glad.git          11f33ab7625ada108f836a83cd8abc405bd870e9
 CALL :Clone tinyobjloader   https://github.com/syoyo/tinyobjloader.git   51d13700d880d2d5fdaf5bd3547100bafcae7932
 CALL :Clone simpleini       https://github.com/brofield/simpleini        2af65fcc504f8242752755e836709762ef7ce062
