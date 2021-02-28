1c7177c3e96438e233d3cc46ea5e2df996c6173b
Ian Dunn <iondune@gmail.com>
2021-02-28 12:19:39 -0800

    [Dependencies] Update tronkko/dirent to latest

--- Dependencies/Clone.bat
+++ Dependencies/Clone.bat
@@ -8,8 +8,7 @@ CALL :Clone imgui           https://github.com/ocornut/imgui.git         4b4be11
 CALL :Clone doctest         https://github.com/onqtam/doctest.git        f0ab4e793fa863d4a5929ff263ddd3bebec8767e
 CALL :Clone tinyobjloader   https://github.com/syoyo/tinyobjloader.git   51d13700d880d2d5fdaf5bd3547100bafcae7932
 CALL :Clone simpleini       https://github.com/brofield/simpleini.git    7bca74f6535a37846162383e52071f380c99a43a
-
-CALL :Clone dirent          https://github.com/tronkko/dirent.git        94ff8bdbb4e11eea5fba17b92edfcdc4b4ece769
+CALL :Clone dirent          https://github.com/tronkko/dirent.git        833b6928e860578b0237fb0a3c04277b9c0c2447
 
 EXIT /B 0
 
